8c7007a65e56c6cfac72f464ad0261411fec2506
sislecv <sislecv@outlook.com>
2026-02-13 17:16:12 +0800

    Update install.sh
    add npmmirror

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ pkg update -y && pkg upgrade -y
 pkg install proot -y
 echo "proot installed"
 pkg install nodejs-lts python make build-essential git -y
-npm install -g openclaw@latest
+npm install -g openclaw@latest --registry=https://registry.npmmirror.com
 curl -fssl https://cdn.jsdelivr.net/gh/Sislecv/openclaw-on-termux@refs/heads/main/firstrun.sh
 echo "openclaw installed.now run termux-chroot and bash firstrun.sh"
