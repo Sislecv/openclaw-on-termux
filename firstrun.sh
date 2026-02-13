e0e15d51aba1f149cff362475c03f4d6967f0074
sislecv <sislecv@outlook.com>
2026-02-13 17:10:40 +0800

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -1,4 +1,4 @@
-curl -fSSL https://raw.githubusercontent.com/Sislecv/openclaw-on-termux/refs/heads/main/bot.sh
+curl -fSSL https://cdn.jsdelivr.net/gh/Sislecv/openclaw-on-termux@refs/heads/main/bot.sh
 openclaw onboard
 echo "copy the link"
 openclaw dashboard --no-open
