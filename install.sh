d7afa1909bfcb1f489def0e7206a7ff534d25975
sislecv <sislecv@outlook.com>
2026-02-13 17:11:27 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ pkg install proot -y
 echo "proot installed"
 pkg install nodejs-lts python make build-essential git -y
 npm install -g openclaw@latest
-curl -fssl https://raw.githubusercontent.com/Sislecv/openclaw-on-termux/refs/heads/main/firstrun.sh
+curl -fssl https://cdn.jsdelivr.net/gh/Sislecv/openclaw-on-termux@refs/heads/main/firstrun.sh
 echo "openclaw installed.now run termux-chroot and bash firstrun.sh"
