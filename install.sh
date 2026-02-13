52cace0a5a25d275af2afd91b4d7ab463f406c64
sislecv <sislecv@outlook.com>
2026-02-13 15:54:20 +0800

    Update install.sh
    Add firstrun.sh downloader

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ pkg update && pkg upgrade
 pkg install proot
 pkg install nodejs-lts python make build-essential git -y
 npm install -g openclaw@latest
-echo "openclaw installed.now run termux-chroot"
+curl -fssl -o https://raw.githubusercontent.com/Sislecv/openclaw-on-termux/refs/heads/main/firstrun.sh
+echo "openclaw installed.now run termux-chroot and bash firstrun.sh"
