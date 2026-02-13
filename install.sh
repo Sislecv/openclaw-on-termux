cf3475739ecfe7c9e306d0f952e01d8992f1090a
sislecv <sislecv@outlook.com>
2026-02-13 16:27:53 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 termux-change-repo
 termux-setup-storage
-pkg update && pkg upgrade
-pkg install proot
+pkg update -y && pkg upgrade -y
+pkg install proot -y
+echo "proot installed"
 pkg install nodejs-lts python make build-essential git -y
 npm install -g openclaw@latest
 curl -fssl https://raw.githubusercontent.com/Sislecv/openclaw-on-termux/refs/heads/main/firstrun.sh
